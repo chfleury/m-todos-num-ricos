6b917a6f9823e03c55bcdc908bb6fa789939ac9b
Christian Fleury <chfleurysiq@gmail.com>
2022-04-09 17:33:49 -0300

    feat: trapezio simples

---
+++ trapezio.py
@@ -0,0 +1,17 @@
+import math
+
+def f(x):
+  return (x**2) * (math.e**(-x))  # x^2e^-x dx
+  
+def trapezio(a, b):
+  x0 = a
+  x1 = b
+  h = b - a
+
+  # (f(x0) + f(x1)) * (h/2)
+  return (f(x0) + f(x1)) * (h/2)
+
+a = 0
+b = 1
+
+print(round(trapezio(a, b), 4))
