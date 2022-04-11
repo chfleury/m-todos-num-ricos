e478b11f645f41498bc287041f8d0b69f1f17b77
Christian Fleury <chfleurysiq@gmail.com>
2022-04-11 18:18:19 -0300

    feat: add composite Trapezoidal

---
+++ compositeTrapezoidal.py
@@ -0,0 +1,24 @@
+import math
+
+def f(x):
+  return  1/(x**2) # 1/x² dx
+
+def compositeTrapezoidal(a, b, n):
+  x0 = a
+  xn = b
+  h = (b - a)/n
+
+  xi = []
+
+  for i in range(1, int((b-a)/h)):
+    xi.append(a + (i*h))
+
+  print(xi)
+  # (f(x0) + f(x1)) * (h/2)
+  return (f(x0) + 2*sum([f(i) for i in xi]) + f(xn)) * (h/2)
+
+a = 1
+b = 7
+n = 6
+
+print(round(compositeTrapezoidal(a, b, n), 4))
