fd473a0c200672b6b86c2946a96a078b54f8e6aa
Christian Fleury <chfleurysiq@gmail.com>
2022-04-11 18:18:39 -0300

    refactor: make commentary fancier

--- simpsons13.py
+++ simpsons13.py
@@ -1,8 +1,8 @@
 import math
 
 def f(x):
-  return (x**2) * (math.e**(-x))  # x^2e^-x 
-  
+  return (x**2) * (math.e**(-x))  # x^2e^-x
+
 def simpsons13(a, b):
   x0 = a
   x2 = b
@@ -10,9 +10,9 @@ def simpsons13(a, b):
   h = (b - a)/2
 
   # (f(x0) + f(x1)) * (h/2)
-  return (h/3)*( f(x0) + 4*(f(x1)) + f(x2) ) 
+  return (h/3)*( f(x0) + 4*(f(x1)) + f(x2) )
 
 a = 0
 b = 1
- 
+
 print(round(simpsons13(a, b), 4))
